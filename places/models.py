038d8bea377eb34c62fef5f6167dc570ea898d9b
gennadynemchin@gmail.com <gennadynemchin@gmail.com>
2023-05-16 10:05:38 +0300

    Added tinymce to admin panel;

--- places/models.py
+++ places/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.utils.html import mark_safe
+from tinymce.models import HTMLField
 
 
 class Place(models.Model):
     title = models.CharField('Title', max_length=200)
     description_short = models.CharField('Description', max_length=300)
-    description_long = models.TextField('Full description')
+    description_long = HTMLField('Full description')
     lat = models.FloatField(verbose_name='Latitude')
     lon = models.FloatField(verbose_name='Longitude')
 
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from where_to_go.views import index, place_detail
 from django.conf import settings
 from django.conf.urls.static import static
@@ -9,4 +9,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
     path('places/<int:place>/', place_detail, name='places'),
+    path('tinymce/', include('tinymce.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
