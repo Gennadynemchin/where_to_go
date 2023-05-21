a43b110e0606d6e03d6899c6177a983044c822a4
gennadynemchin@gmail.com <gennadynemchin@gmail.com>
2023-05-21 19:16:23 +0300

    Refactoring;

--- places/models.py
+++ places/models.py
@@ -5,7 +5,7 @@ from tinymce.models import HTMLField
 
 class Place(models.Model):
     title = models.CharField("Title", max_length=200)
-    description_short = models.CharField("Description", max_length=300)
+    description_short = models.TextField("Description")
     description_long = HTMLField("Full description")
     lat = models.FloatField(verbose_name="Latitude")
     lon = models.FloatField(verbose_name="Longitude")
