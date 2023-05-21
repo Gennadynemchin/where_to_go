9a16dca7193654a692ce1007ba7a1d257b8daef2
gennadynemchin@gmail.com <gennadynemchin@gmail.com>
2023-05-21 22:48:54 +0300

    Added blank=True for model Place -> description;

--- places/admin.py
+++ places/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from .models import Place, Image
+from django.utils.html import mark_safe
 from adminsortable2.admin import SortableStackedInline, SortableAdminBase
 
 
 class ImageStackedInline(SortableStackedInline):
     model = Image
     readonly_fields = ["image_preview"]
+
+    def image_preview(self, obj):
+        return mark_safe(f'<img src = "{obj.image.url}" height = "150"/>')
 
 
 @admin.register(Place)
--- places/models.py
+++ places/models.py
@@ -1,12 +1,11 @@
 from django.db import models
-from django.utils.html import mark_safe
 from tinymce.models import HTMLField
 
 
 class Place(models.Model):
     title = models.CharField("Title", max_length=200)
-    description_short = models.TextField("Description")
-    description_long = HTMLField("Full description")
+    description_short = models.TextField("Description", blank=True)
+    description_long = HTMLField("Full description", blank=True)
     lat = models.FloatField(verbose_name="Latitude")
     lon = models.FloatField(verbose_name="Longitude")
 
@@ -23,9 +22,6 @@ class Image(models.Model):
 
     class Meta:
         ordering = ["position"]
-
-    def image_preview(self):
-        return mark_safe(f'<img src = "{self.image.url}" height = "150"/>')
 
     def __str__(self):
         return self.place.title
