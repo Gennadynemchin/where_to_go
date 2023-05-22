86d8757208b895a799dd407b50fa54e55ce64b26
gennadynemchin@gmail.com <gennadynemchin@gmail.com>
2023-05-22 17:59:19 +0300

    Refactoring of views/load_place;

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -14,32 +14,38 @@ class Command(BaseCommand):
         response = requests.get(options["download_link"][0])
         response.raise_for_status()
         content = response.json()
+        place_images = content.get("imgs", [])
+        place_desc_short = content.get("description_short", "Short description should be filled out later")
+        place_desc_long = content.get("description_long", "Full description should be filled out later")
         try:
-            images = content["imgs"]
-            place, created = Place.objects.get_or_create(
-                title=content["title"],
-                description_short=content["description_short"],
-                description_long=content["description_long"],
-                lat=content["coordinates"]["lat"],
-                lon=content["coordinates"]["lng"],
-            )
-            self.stdout.write(
-                self.style.SUCCESS(f'Successfully saved place {content["title"]}')
-            )
-
-            for image_count, image_url in enumerate(images):
-                image_request = requests.get(image_url)
-                image_request.raise_for_status()
-                image_file = ContentFile(image_request.content)
-                image_name = os.path.basename(urlparse(image_url).path)
-                new_image, created = Image.objects.get_or_create(
-                    position=image_count, place=place
-                )
-                new_image.image.save(content=image_file, name=image_name, save=True)
-                self.stdout.write(
-                    self.style.SUCCESS(
-                        f'Successfully saved image {image_name} for place {content["title"]}'
-                    )
-                )
+            place_title = content["title"]
+            place_lat = content["coordinates"]["lat"]
+            place_lng = content["coordinates"]["lng"]
         except KeyError:
-            self.stdout.write(self.style.WARNING("Wrong JSON format!"))
+            self.stdout.write(self.style.WARNING("Wrong JSON format. The place will no be added to database."))
+        else:
+            place, created = Place.objects.get_or_create(
+                title=place_title,
+                description_short=place_desc_short,
+                description_long=place_desc_long,
+                lat=place_lat,
+                lon=place_lng,
+            )
+            if place_images:
+                for image_count, image_url in enumerate(place_images):
+                    image_request = requests.get(image_url)
+                    image_request.raise_for_status()
+                    image_file = ContentFile(image_request.content)
+                    image_name = os.path.basename(urlparse(image_url).path)
+                    new_image, created = Image.objects.get_or_create(
+                        position=image_count, place=place
+                    )
+                    new_image.image.save(content=image_file, name=image_name, save=True)
+                    self.stdout.write(
+                        self.style.SUCCESS(
+                            f'Successfully saved image {image_name} for place {content["title"]}'
+                        )
+                    )
+        self.stdout.write(
+            self.style.SUCCESS(f'Successfully saved place {content["title"]}')
+        )
